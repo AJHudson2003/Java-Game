cb635e226ec4a9809dcfdd1e4f67cf712487d1eb
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-11-18 09:59:17 -0500

    added input functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //declares the object and initialize with
+        //predefined standard input object
+        Scanner sc = new Scanner(System.in);
+        //String input
+        String name = sc.nextLine();
+        //Character input
+        char gender = sc.next().charAt(0);
+        //numerical data input
+        //byte, short and float can be read
+        //using similar-named function
+        int age = sc.nextInt();
+        long mobileNO = sc.nextLong();
     }
 }
