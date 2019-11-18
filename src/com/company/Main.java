4ac8401a99788cf391c283923d35305957eb2fc5
AJHudson2003 <48022408+AJHudson2003@users.noreply.github.com>
2019-11-18 12:27:03 -0500

    added more input for user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,5 +16,29 @@ public class Main {
         //using similar-named function
         int age = sc.nextInt();
         long mobileNO = sc.nextLong();
+	double credit = sc.nextDouble();
+	// Declare an object and initialize with 
+        // predefined standard input object 
+        Scanner sc = new Scanner(System.in); 
+  
+        // Initialize sum and count of input elements 
+        int sum = 0, count = 0; 
+  
+        // Check if an int value is available 
+        while (sc.hasNextInt()) 
+        { 
+            // Read an int value 
+            int num = sc.nextInt(); 
+            sum += num; 
+            count++; 
+        } 
+        int mean = sum / count; 
+	// Print the values to check if the input was correctly obtained. 
+        System.out.println("Name: "+name); 
+        System.out.println("Gender: "+gender); 
+        System.out.println("Age: "+age); 
+        System.out.println("Mobile Number: "+mobileNo); 
+        System.out.println("Credit: "+credit); 
+	 System.out.println("Mean: " + mean)
     }
 }
