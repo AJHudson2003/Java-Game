04bf76285c78cc372692f6582d24d60e709e0235
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-11-19 09:28:37 -0500

    added a sum input and gender input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,16 @@ public class Main {
 	    //declares the object and initialize with
         //predefined standard input object
         Scanner sc = new Scanner(System.in);
-        //String input
+        //name code
         String name = sc.nextLine();
-       System.out.println("name"+ name);
+       System.out.println("name: "+ name);
+       // gender code
        String gender = sc.nextLine();
-       System.out.println("gender"+ gender);
+       System.out.println("gender: "+ gender);
+
         int sum = 0, count = 0;
 
-        // Check if an int value is available
+
         while (sc.hasNextInt())
         {
             // Read an int value
