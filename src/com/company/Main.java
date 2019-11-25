dc8302a965c478a0084f27c201bde8d64a26407c
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-11-25 08:55:53 -0500

    added exam passer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,24 +4,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	 //declares the object and initialize with
+        //declares the object and initialize with
         //predefined standard input object
         Scanner sc = new Scanner(System.in);
         //name code
-        string name = sc.nextLine();
-	System.out.println("enter your name: ");
-       	System.out.println("name: "+ name);
-       	// gender code
-       	string gender = sc.nextLine();
-	System.out.println(" enter you gender here: ");
-        System.out.println("gender: "+ gender);
-	    
-	system.out.println("enter you grades and after you do type end and enter to emd the line of code");
+        System.out.println("enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("name: " + name);
+        // gender code
+        System.out.println(" enter you gender here: ");
+        String gender = sc.nextLine();
+        System.out.println("gender: " + gender);
+
+        System.out.println("enter you grades and after you do type end and enter to end the line of code");
         int sum = 0, count = 0;
 
 
-        while (sc.hasNextInt())
-        {
+        while (sc.hasNextInt()) {
             // Read an int value
             int num = sc.nextInt();
             sum += num;
@@ -29,7 +28,22 @@ public class Main {
         }
         int grade = sum / count;
         System.out.println("average: " + grade);
-	    
-	
+
+        int marksObtained, passingMarks;
+
+        passingMarks = 60;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input marks scored by you:");
+
+        marksObtained = input.nextInt();
+
+        if (marksObtained >= passingMarks) {
+            System.out.println("You passed the exam.");
+        } else {
+            System.out.println("Unfortunately, you failed to pass the exam.");
+
+        }
     }
 }
