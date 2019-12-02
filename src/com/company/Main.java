50ceeaea0d3c02d76c9c0fd52ea424f21f7bface
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2019-12-02 09:18:57 -0500

    fixed errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,15 @@ public class Main {
         //predefined standard input object
         Scanner sc = new Scanner(System.in);
         //name code
-        string name = sc.nextLine();
+        String name = sc.nextLine();
 	System.out.println("enter your name: ");
        	System.out.println("name: "+ name);
        	// gender code
-       	string gender = sc.nextLine();
+       	String gender = sc.nextLine();
 	System.out.println(" enter you gender here: ");
         System.out.println("gender: "+ gender);
 	    
-	system.out.println("enter you grades and after you do type end and enter to emd the line of code");
+	System.out.println("enter you grades and after you do type end and enter to emd the line of code");
         int sum = 0, count = 0;
 
 
